2d25058deba093f58da75092bc4b505e1fa23e2f
SomeRandomGithub-User <73361689+SomeRandomGithub-User@users.noreply.github.com>
2022-07-27 10:00:46 -0400

    Create YBAUseRibWithoutRoka.lua

---
+++ YBAUseRibWithoutRoka.lua
@@ -0,0 +1,42 @@
+local player = game:GetService("Players").LocalPlayerlocal function useRib()
+    local args = {
+    [1] = "EndDialogue",
+    [2] = {
+        ["NPC"] = "Rib Cage of The Saint's Corpse",
+        ["Option"] = "Option1",
+        ["Dialogue"] = "Dialogue2"
+    }
+}
+
+game:GetService("Players").LocalPlayer.Character.RemoteEvent:FireServer(unpack(args))
+end
+
+local function getWorthi()
+    local args = {
+    [1] = "LearnSkill",
+    [2] = {
+        ["Skill"] = "Worthiness V",
+        ["SkillTreeType"] = "Character"
+    }
+}
+game:GetService("Players").LocalPlayer.Character.RemoteFunction:InvokeServer(unpack(args))
+end
+
+local function equipStand()
+wait(1)
+local args = {
+    [1] = "ToggleStand",
+    [2] = "Toggle"
+}
+game:GetService("Players").LocalPlayer.Character.RemoteFunction:InvokeServer(unpack(args))
+local player = game:GetService("Players").LocalPlayer
+local stand = player.PlayerStats.Stand.Value
+print(stand)
+end
+
+--------------------------------------------------------
+useRib()
+getWorthi()
+wait(5)
+equipStand()
+equipStand()
